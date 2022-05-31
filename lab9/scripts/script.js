b974c914921a1921b0f1e0cf48be9303b5891a31
unknown <maksymchukmaxym@gmail.com>
2022-06-01 00:24:24 +0300

    done lab9

--- lab9/scripts/script.js
+++ lab9/scripts/script.js
@@ -1,114 +1,104 @@
 (function () {
     $ajaxUtils.sendGetRequest(
-        "snippets/navigation-snippet.html",
-        function (response) {
-            document.getElementById("navigation").innerHTML = response
+        "snippets/header.snippet.html",
+        response => {
+            document.getElementById("header").innerHTML = response
         });
     loadCategories();
 }());
-
-function showLoader(show) {
-    document.getElementById("loader").innerHTML = show ? `<div class="loader-wrapper"><div class="loader"></div></div>` : ``;
-}
 
 function insertProperty(html, propName, propValue) {
     return html.replace(new RegExp(`{{${propName}}}`, "g"), propValue);
 }
 
+
 function mainContentClear() {
     document.getElementById("main-content").innerHTML =
         `<div id="category-name"></div>
-        <div class="row justify-content-center" id="categories"></div>
-    <div id="categories-specials"></div>`;
+         <div class="row justify-content-center" id="categories"></div>
+         <div id="category-special"></div>`;
 }
 
 function loadCategories() {
-    showLoader(true);
     mainContentClear();
     $ajaxUtils.sendGetRequest(
         "storage/categories.json",
-        function (response) {
+        categoriesResp => {
             $ajaxUtils.sendGetRequest(
-                "snippets/category-item-snippet.html",
-                function (responseHtml) {
-                    var categories = JSON.parse(response).categories;
-                    for (let i = 0; i < categories.length; i++) {
-                        var html = responseHtml;
-                        html = insertProperty(html, "name", categories[i].name);
-                        html = insertProperty(html, "image", categories[i].image);
-                        html = insertProperty(html, "notes", categories[i].notes);
-                        html = insertProperty(html, "shortname", categories[i].shortname);
+                "snippets/category-item.snippet.html",
+                catSnippet => {
+                    var categories = JSON.parse(categoriesResp);
+                    categories.forEach(ct => {
+                        var html = catSnippet;
+                        html = insertProperty(html, "name", ct.name);
+                        html = insertProperty(html, "image", ct.image);
+                        html = insertProperty(html, "id", ct.id);
+                        html = insertProperty(html, "notes", ct.notes);
+                        html = insertProperty(html, "shortname", ct.shortname);
                         document.getElementById("categories").innerHTML += html
-                    }
-                    showLoader(false);
+
+                    });
                 });
         });
     $ajaxUtils.sendGetRequest(
-        "snippets/specials-snippet.html",
-        function (response) {
-            document.getElementById("categories-specials").innerHTML = response
+        "snippets/specials.snippet.html",
+        specialsSnippet => {
+            document.getElementById("category-special").innerHTML = specialsSnippet
         });
 }
 
-function loadCatalog() {
-    showLoader(true);
+function loadCategory(id) {
     mainContentClear();
     $ajaxUtils.sendGetRequest(
         "storage/categories.json",
-        function (response) {
-            var categories = JSON.parse(response).categories;
-            for (let i = 0; i < categories.length; i++) {
-                loadCategory(categories[i].shortname, true)
-            }
-            showLoader(false);
+        categoriesJson => {
+            var categories = JSON.parse(categoriesJson);
+            var selectedCategory = categories.filter(c => c.id == id)[0];
+
+            $ajaxUtils.sendGetRequest(
+                "snippets/category-header.snippet.html",
+                categoryHeaderSnippet => {
+                    categoryHeaderSnippet = insertProperty(categoryHeaderSnippet, "name", selectedCategory.name);
+                    document.getElementById("category-name").innerHTML = categoryHeaderSnippet
+                });
+            loadCategoryItems(selectedCategory);
         });
+
 }
 
 function loadSpecials() {
-    var randCategory = Math.floor(Math.random() * (6 - 1) + 1);
-    loadCategory(`cat${randCategory}`)
+    var randCategory = Math.floor(Math.random() * (4-1)+1);
+    loadCategory(randCategory);
 }
 
-function loadCategoryName(shortname) {
-    showLoader(true);
+function loadCatalog() {
+    mainContentClear();
     $ajaxUtils.sendGetRequest(
-        "snippets/category-name-snippet.html",
-        function (responseHtml) {
-            $ajaxUtils.sendGetRequest(
-                "storage/categories.json",
-                function (response) {
-                    var category = JSON.parse(response).categories.filter(e => e.shortname === shortname);
-                    responseHtml = insertProperty(responseHtml, "name", category[0].name);
-                    document.getElementById("category-name").innerHTML = responseHtml
-                    showLoader(false);
-                });
+        "storage/categories.json",
+        function (response) {
+            var categories = JSON.parse(response);
+            categories.forEach(c => {
+                loadCategoryItems(c);
+            });
         });
 }
 
-function loadCategory(shortname, isShowCategoryName) {
-    showLoader(true);
-    mainContentClear();
-    if (!isShowCategoryName) { loadCategoryName(shortname) }
+function loadCategoryItems(category) {
     $ajaxUtils.sendGetRequest(
-        `storage/goods/${shortname}-goods.json`,
-        function (response) {
+        "snippets/item.snippet.html",
+        itemSnippet => {
             $ajaxUtils.sendGetRequest(
-                "snippets/goods-item-snippet.html",
-                function (responseHtml) {
-                    var goods = JSON.parse(response).goods;
-                    for (let i = 0; i < goods.length; i++) {
-                        var html = responseHtml;
-                        html = insertProperty(html, "name", goods[i].name);
-                        html = insertProperty(html, "image", goods[i].image);
-
-                        if (goods[i].description.length > 100) {
-                            goods[i].description = `${goods[i].description.slice(0, 100)}...`;
-                        }
-                        html = insertProperty(html, "description", goods[i].description);
-                        html = insertProperty(html, "price", goods[i].price);
+                `storage/items/${category.shortname}-items.json`,
+                catItemsJson => {
+                    var catItems = JSON.parse(catItemsJson);
+                    catItems.forEach(i => {
+                        var html = itemSnippet;
+                        html = insertProperty(html, "name", i.name);
+                        html = insertProperty(html, "image", i.image);
+                        html = insertProperty(html, "description", i.description);
+                        html = insertProperty(html, "price", i.price);
                         document.getElementById("categories").innerHTML += html
-                    }
-                    showLoader(false);
+                    });
                 });
         });
 }
